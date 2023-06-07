72890317e22a18d73faab2ea1fa66e42266437d5
bouimust <moussovic@gmail.com>
2023-06-07 12:45:56 +0200

    create edit/new property page

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+
+
+    // Shows all properties to manage (visible or not)
+    #[Route('/admin', name: 'admin.property.index')]
+    public function index(): Response
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
+    }
+
+
+
+    // Create new property
+    /**
+     * @param Request $request
+     */
+    #[Route('/admin/biens/nouveau', name: 'admin.property.new')]
+    public function new(Request $request): Response
+    {
+        // create empty property
+        $property = new Property();
+
+        // create Property form
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        // check if form is submitted and valid, so save in DB and redirect to index
+        if ($form->isSubmitted() && $form->isValid()) {
+            // the property entity must be tracked by EntityManager
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView() // create view for the template
+        ]);
+    }
+
+
+
+    // Edit one property
+    /**
+     * @param Property $property
+     * @param Request $request
+     */
+    #[Route('/admin/biens/{id}', name: 'admin.property.edit', methods: ['GET', 'POST'])]
+    public function edit(Property $property, Request $request): Response
+    {
+        // create Property form and pass $property datas to fill form
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        // check if form is submitted and valid, so save in DB and redirect to index
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/edit.html.twig', [
+            'propoerty' => $property,
+            'form' => $form->createView() // create view for the template
+        ]);
+    }
+
+    // Delete one property
+    /**
+     * @param Property $property
+     */
+    #[Route('/admin/biens/{id}/delete', name: 'admin.property.delete', methods: 'POST')]
+    public function delete(Property $property): Response
+    {
+        dd($property, 'supprimé');
+        $this->em->remove($property);
+        $this->em->flush();
+        return $this->redirectToRoute('admin.property.index');
+    }
+}
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', null, ['label' => 'Titre'])  // this is a way to translate
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat', ChoiceType::class, ['choices' => $this->getChoices()]) // display heat name instead of int
+            ->add('city')
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold')
+            //->add('created_at')  Not need for the form
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+
+    // Get heat name instead of int
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $result = [];
+        foreach ($choices as $key => $value) {
+            $result[$value] = $key;
+        }
+        return $result;
+    }
+}
