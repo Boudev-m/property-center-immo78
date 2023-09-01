d674f6d0ab20a425b51b6375b6e75be0d74f92ed
bouimust <moussovic@gmail.com>
2023-09-01 09:52:29 +0200

    create admin account

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,10 +4,11 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements FixtureGroupInterface
 {
     /**
      * @var UserPasswordHasherInterface
@@ -23,9 +24,16 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $user = new User;
-        $user->setUsername('demo');
-        $user->setPassword($this->hasher->hashPassword($user, 'mode'));
+        $user->setUsername('admin');
+        $user->setPassword($this->hasher->hashPassword($user, 'admin729'));
         $manager->persist($user);
         $manager->flush();
+    }
+
+    // Can load only news fixture with doctrine:fixtures:load --group=USER
+    // Fixture must implement FixtureGroupInterface
+    public static function getGroups(): array
+    {
+        return ['USER'];
     }
 }
